747b0769a3e256551f8e20c694d43a0d152256aa
Krzysztof Jastrzębski <krzysztof@jastrzebski>
2023-11-26 09:00:03 +0100

    first commit

---
+++ P11BlazorWebAssembly.Client/Models/Card.cs
@@ -0,0 +1,8 @@
+public class Card
+{
+    public int Id { get; set; }
+    public string ImageUrl { get; set; }
+    public string Title { get; set; }
+
+    public string Director { get; set;}
+}
---
+++ P11BlazorWebAssembly.Client/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using P06Shop.Shared.Configuration;
+using P06Shop.Shared.Services.MovieService;
+
+using P11BlazorWebAssembly.Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+var appSettings = builder.Configuration.GetSection(nameof(AppSettings));
+var appSettingsSection = appSettings.Get<AppSettings>();
+
+
+var uriBuilderMovies = new UriBuilder(appSettingsSection.BaseAPIUrl)
+{
+    Path = appSettingsSection.BaseMovieEndpoint.Base_url,
+};
+//Microsoft.Extensions.Http
+builder.Services.AddHttpClient<IMovieService, MovieService>(client => client.BaseAddress = uriBuilderMovies.Uri);
+
+builder.Services.AddSingleton<IOptions<AppSettings>>(new OptionsWrapper<AppSettings>(appSettingsSection));
+
+
+await builder.Build().RunAsync();
